4db7358c0eb5a91aca7d2e06a1705c1a2a6b8e48
likewagon <likewagon@outlook.com>
2021-06-30 14:19:06 +0500

    stepper add

---
+++ components/UI/Stepper.tsx
@@ -0,0 +1,57 @@
+import { Step, Stepper } from 'react-form-stepper'
+
+const FormStepper = (props) => {
+  // const {steps, activeStep} = props
+  const { activeStep, className } = props
+  const steps = [
+    {
+      label: 'first',
+    },
+    {
+      label: 'second',
+    },
+    {
+      label: 'third',
+    },
+    {
+      label: 'forth',
+    },
+    {
+      label: 'fifth',
+    },
+  ]
+
+  return (
+    <Stepper
+      activeStep={activeStep}
+      className={className}
+      styleConfig={{
+        activeBgColor: '#ed1d24',
+        activeTextColor: '#ffffff',
+        completedBgColor: '#a10308',
+        completedTextColor: '#ffffff',
+        inactiveBgColor: '#e0e0e0',
+        inactiveTextColor: '#ffffff',
+        size: '1.5em',
+        circleFontSize: '1rem',
+        labelFontSize: '0.875rem',
+        borderRadius: '50%',
+        fontWeight: 500,
+      }}
+      connectorStyleConfig={{
+        size: 1,
+        stepSize: '2em',
+        disabledColor: '#bdbdbd',
+        activeColor: '#ed1d24',
+        completedColor: 'a10308',
+        style: 'solid',
+      }}
+    >
+      {steps.map((step, index) => (
+        <Step key={index} label={step.label} className="p-0"/>
+      ))}
+    </Stepper>
+  )
+}
+
+export default FormStepper
