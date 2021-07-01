81334aa9cbc0fe1575e49a741bf8947d176884db
jetsoanalin <jetyjetso@gmail.com>
2021-07-01 05:34:06 +0530

    Mainnet Addresses Added

--- constant/blockchain.tsx
+++ constant/blockchain.tsx
@@ -1,14 +1,31 @@
-const contractAdd =  "0x60c322865eb985c340B92b3927a8E9b9cAe5b4f9";
-const contractAddB = "0x8930b9977D3a04D1B8696994417E7F45a94CEF5e";
+/* Testnet Address */
 
-const tokencontractAdd = "0xBb80b2932d982a61C0Dffa6c1Af5531EEBD48cBe";
-const tokencontractAddB = "0x09b57aA9F052165a98Dcc06e3c380e5BD29a497f";
+// const contractAdd =  "0x60c322865eb985c340B92b3927a8E9b9cAe5b4f9";
+// const contractAddB = "0x8930b9977D3a04D1B8696994417E7F45a94CEF5e";
 
-const marketcontractAdd = "0xc4E523243551359d71a2f2986D0EC3c388e7191B";
-const marketcontractAddB = "0xF9a34a112357959a465b6d250dab294EDF69B72e";
+// const tokencontractAdd = "0xBb80b2932d982a61C0Dffa6c1Af5531EEBD48cBe";
+// const tokencontractAddB = "0x09b57aA9F052165a98Dcc06e3c380e5BD29a497f";
 
-const tokenhandlerAdd = "0x6e598F393532b5de8751d1A91c35eC33e4fEc09E";
-const tokenhandlerAddB = "0xb89FbeC6199Ba9251aD2Ec18e5daa4E47A54C794";
+// const marketcontractAdd = "0xc4E523243551359d71a2f2986D0EC3c388e7191B";
+// const marketcontractAddB = "0xF9a34a112357959a465b6d250dab294EDF69B72e";
+
+// const tokenhandlerAdd = "0x6e598F393532b5de8751d1A91c35eC33e4fEc09E";
+// const tokenhandlerAddB = "0xb89FbeC6199Ba9251aD2Ec18e5daa4E47A54C794";
+
+/* Mainnet Address */
+
+const contractAdd =  "0x7432fD78B43D21E24371935dA68290016E961F46";
+const contractAddB = "0x4552db0E6d50e215783eA1AE3909c347c5Af1513";
+
+const tokencontractAdd = "0x9ea1ae46c15a4164b74463bc26f8aa3b0eea2e6e";
+const tokencontractAddB = "0x0f66a7afdced2d664ba8988b4d2b722f7a459106";
+
+const marketcontractAdd = "0x8FE5cA93dab7FB6A559c28A01c85fE3DACb14834";
+const marketcontractAddB = "0xBBB26D01E5ED6EB372615113A3a6f0F66B581B58";
+
+const tokenhandlerAdd = "0x0De7FA72d44b7f3B177A2A28EA9034214BC0e748";
+const tokenhandlerAddB = "0xADe4A4B1853A28bb6b6882FB5c3b41cd98B83503";
+  
   
 export { contractAdd,contractAddB,tokencontractAdd,tokencontractAddB,marketcontractAdd,marketcontractAddB };
   
